ed9b655ae556927646ab30ce4086da38fd8c7d96
Дмитрий Заречнев <zarechnevd@gmail.com>
2022-10-17 17:41:09 +0300

    update ui for bloc example

--- bloc/lib/common/ui/app_colors.dart
+++ bloc/lib/common/ui/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Colors.black;
   static const textPrimary = Colors.black;
+  static const accent = Colors.blue;
 }
